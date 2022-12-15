c50d1f08702fa4d9e7df75faf3238f7b745c7f11
Jeffery Walsh <cyberhorsey@gmail.com>
2022-12-14 18:54:53 -0800

    rollback changes to config object

--- config/classNames.js
+++ config/classNames.js
@@ -1,8 +1,10 @@
-const classConfig = {
+const config = {
+    classNames: {
         MageClassName: "mage",
         ShamanClassName: "shaman",
         WarlockClassName: "warlock"
-    
+    },
+    gameName: "Horseman 5000 Game"
 }
 
 module.exports = config;
